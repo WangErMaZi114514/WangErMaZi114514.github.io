8b8610b8fca9cb012e99fe6ac60b9c986d10cdbe
WangErMaZi <593199538@qq.com>
2022-12-10 14:18:15 +0800

    Update res.js

---
+++ ebook/code/res.js
@@ -0,0 +1,14 @@
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+ }
+ function setImageHref(){
+    grade = getQueryString('g');
+    book = getQueryString('b');
+    page = getQueryString('p');
+    pageIMG.href = 'https://wangermazi114514.github.io/ebook/book_res/'+grade+'/'+book+'/'+page+'.jpg';
+ }
